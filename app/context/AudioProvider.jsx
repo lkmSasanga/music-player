02a90c9ce9fde5506a8e908d9dbc8595939d77a2
MalinduSasanga <lkmsasanga@gmail.com>
2021-08-01 09:41:00 +0530

    get access to audio

--- app/context/AudioProvider.jsx
+++ app/context/AudioProvider.jsx
@@ -1,65 +1,74 @@
-import React, { Component } from 'react'
-import { Alert, PermissionsAndroid, Text, View } from 'react-native'
-import * as MediaLibrary from 'expo-media-library'
+import React, { Component, createContext } from "react";
+import { Alert, PermissionsAndroid, Text, View } from "react-native";
+import * as MediaLibrary from "expo-media-library";
 
+export const AudioContext = createContext();
 export default class AudioProvider extends Component {
+  constructor(props) {
+    super(props);
+  }
 
+  permissionAlert = () => {
+    Alert.alert("Persmission Required", "This app needs to read audio files!", [
+      {
+        text: "I am ready",
+        onPress: () => this.getPermission(),
+      },
+      {
+        text: "cancel",
+        onPress: () => this.getPermissionAlert(),
+      },
+    ]);
+  };
 
-    constructor(props) {
-        super(props)
+  getAudioFiles = async () => {
+    const media = await MediaLibrary.getAssetsAsync({
+      mediaType: "audio",
+    });
+  };
+
+  getPermission = async () => {
+    // {
+    //     "canAskAgain": true,
+    //     "expires": "never",
+    //     "granted": false,
+    //     "status": "undetermined",
+    //   }
+
+    const permission = await MediaLibrary.getPermissionsAsync();
+    if (permission.granted) {
+      // we want to get all the audio files
+      this.getAudioFiles();
     }
 
-    permissionAlert = () => {
-        Alert.alert('Persmission Required', 'This app needs to read audio files!', [{
-            text: 'I am ready',
-            onPress: () => this.getPermission()
+    if (!permission.granted && permission.canAskAgain) {
+      const { status, canAskAgain } =
+        await MediaLibrary.requestPermissionsAsync();
+      if (status === "denied" && canAskAgain) {
+        // we are going to display aler that user must allow this persmission to work this app
+        this.permissionAlert();
+      }
 
-        }, {
-            text: 'cancel',
-            onPress: () => this.getPermissionAlert()
-        }
-        ])
+      if (status === "granted") {
+        // we want to get all the audio files
+        this.getAudioFiles();
+      }
+
+      if (status === "denied" && !canAskAgain) {
+        // we want to display some error to user
+      }
     }
+  };
 
-    getPermission = async () => {
-        // {
-        //     "canAskAgain": true,
-        //     "expires": "never",
-        //     "granted": false,
-        //     "status": "undetermined",
-        //   }
+  componentDidMount() {
+    this.getPermission();
+  }
 
-        const permission = await MediaLibrary.getPermissionsAsync()
-        if (permission.granted) {
-            // we want to get all the audio files
-        }
-
-        if (!permission.granted && permission.canAskAgain) {
-            const { status, canAskAgain } = await MediaLibrary.requestPermissionsAsync()
-            if (status === 'denied' && canAskAgain) {
-                // we are going to display aler that user must allow this persmission to work this app
-                this.permissionAlert()
-            }
-
-            if (status === 'granted') {
-                // we want to get all the audio files
-            }
-
-            if (status === 'denied' && !canAskAgain) {
-                // we want to display some error to user
-            }
-        }
-    }
-
-    componentDidMount() {
-        getPermission()
-    }
-
-    render() {
-        return (
-            <View>
-                <Text>textInComponet</Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View>
+        <Text>textInComponet</Text>
+      </View>
+    );
+  }
 }
